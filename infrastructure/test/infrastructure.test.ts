92179ac492a8f1c3e66b1dfc4e5c3031e6b524b7
Hamza Ghani <h.u.f.ghani@gmail.com>
2025-04-16 14:06:54 +0000

    add unit tests

--- infrastructure/test/infrastructure.test.ts
+++ infrastructure/test/infrastructure.test.ts
@@ -1,16 +1,74 @@
-// import * as cdk from 'aws-cdk-lib';
-// import { Template } from 'aws-cdk-lib/assertions';
-// import * as Infrastructure from '../lib/infrastructure-stack';
+import * as cdk from "aws-cdk-lib";
+import { Template } from "aws-cdk-lib/assertions";
+import { SalahSlotsInfrastructureStack } from "../lib/SalahSlotsInfrastructure-stack";
 
-// example test. To run these tests, uncomment this file along with the
-// example resource in lib/infrastructure-stack.ts
-test("SQS Queue Created", () => {
-  //   const app = new cdk.App();
-  //     // WHEN
-  //   const stack = new Infrastructure.InfrastructureStack(app, 'MyTestStack');
-  //     // THEN
-  //   const template = Template.fromStack(stack);
-  //   template.hasResourceProperties('AWS::SQS::Queue', {
-  //     VisibilityTimeout: 300
-  //   });
+describe("SalahSlotsInfrastructureStack", () => {
+  let app: cdk.App;
+  let stack: SalahSlotsInfrastructureStack;
+  let template: Template;
+
+  beforeEach(() => {
+    app = new cdk.App();
+    stack = new SalahSlotsInfrastructureStack(app, "TestStack");
+    template = Template.fromStack(stack);
+  });
+
+  it("should create a Lambda function with the correct properties", () => {
+    template.hasResourceProperties("AWS::Lambda::Function", {
+      Runtime: "nodejs22.x",
+      Handler: "SalahSlotsCalendar.handler",
+      Timeout: 30,
+    });
+  });
+
+  it("should create an IAM role with the correct managed policy", () => {
+    template.hasResourceProperties("AWS::IAM::Role", {
+      AssumeRolePolicyDocument: {
+        Statement: [
+          {
+            Action: "sts:AssumeRole",
+            Effect: "Allow",
+            Principal: {
+              Service: "lambda.amazonaws.com",
+            },
+          },
+        ],
+      },
+      ManagedPolicyArns: [
+        {
+          "Fn::Join": [
+            "",
+            [
+              "arn:",
+              { Ref: "AWS::Partition" },
+              ":iam::aws:policy/service-role/AWSLambdaBasicExecutionRole",
+            ],
+          ],
+        },
+      ],
+    });
+  });
+
+  it("should create a custom cache policy with a 28-day TTL", () => {
+    template.hasResourceProperties("AWS::CloudFront::CachePolicy", {
+      CachePolicyConfig: {
+        DefaultTTL: 2419200, // 28 days in seconds
+        MaxTTL: 2419200,
+        MinTTL: 0,
+        Name: "salah-slots-cache-policy",
+      },
+    });
+  });
+
+  it("should create a Lambda function URL with no authentication", () => {
+    template.hasResourceProperties("AWS::Lambda::Url", {
+      AuthType: "NONE",
+    });
+  });
+
+  it("should output the CloudFront distribution URL", () => {
+    template.hasOutput("CloudFrontURL", {
+      Description: "The CloudFront distribution URL",
+    });
+  });
 });
